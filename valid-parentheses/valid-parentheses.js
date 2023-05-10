a32eb5ee2cde9a03657e334892b7af2f5587785a
Ijay_dev <inyangasi4@gmail.com>
2023-05-10 15:11:24 +0300

    Time: 52 ms (95.85%) | Memory: 42.3 MB (72.22%) - LeetSync

---
+++ valid-parentheses/valid-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+    }
+    for (let i = 0; i < s.length; i++) {
+        if(map[s[i]]) {
+            stack.push(map[s[i]])
+        } else {
+            if (s[i] !== stack.pop()) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
