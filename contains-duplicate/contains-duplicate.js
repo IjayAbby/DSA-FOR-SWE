36748d6d537489b146d45238fee1ce3b5592fc47
Ijay_dev <inyangasi4@gmail.com>
2023-05-10 15:53:49 +0300

    Time: 82 ms (91.09%) | Memory: 55.3 MB (33.25%) - LeetSync

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+   let set = new Set();
+   for (let num of nums) {
+       if (set.has(num)) {
+           return true;
+       }
+       set.add(num);
+   }
+   return false; 
+};
