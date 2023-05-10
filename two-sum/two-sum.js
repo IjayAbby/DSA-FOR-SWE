10cedabfe80478919a82f726807b2b889a3af66d
Ijay_dev <inyangasi4@gmail.com>
2023-05-10 15:30:44 +0300

    Time: 58 ms (92.56%) | Memory: 42.8 MB (39.36%) - LeetSync

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,24 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // let hash_table = {};
-    // for (let i = 0; i < nums.length; i++) {
-    //     let num = nums[i];
-    //     if (hash_table[target - num] !== undefined) {
-    //         return [hash_table[target - num], i]
-    //     }
-    //     hash_table[num] = i;
-    // }
-    // return [];
-
-
-var list = {}
-
-    for (let i=0; i < nums.length; i++){
-        if (list.hasOwnProperty(nums[i])){
-            return [i,list[nums[i]]]
-        }else {
-            list[target - nums[i]] = i
+    let hash_table = {};
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (hash_table[target - num] !== undefined) {
+            return [hash_table[target - num], i]
         }
+        hash_table[num] = i;
     }
+    return [];
 };
