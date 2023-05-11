103ae3331e4233e71fd5a947245dd979aca4583f
Ijay_dev <inyangasi4@gmail.com>
2023-05-11 07:16:58 +0300

    Time: 61 ms (47.69%) | Memory: 42.3 MB (23.84%) - LeetSync

--- remove-element/remove-element.js
+++ remove-element/remove-element.js
@@ -3,18 +3,28 @@
  * @param {number} val
  * @return {number}
  */
+// var removeElement = function(nums, val) {
+//     let count = 0;
+//     for(let i = 0; i < nums.length; i++) {
+//         if(nums[i] === null) {
+//             return count;
+//         }
+//         if(nums[i] === val) {
+//             nums.splice(i, 1);
+//             i--;
+//         } else {
+//             count++;
+//         }
+//     }
+//     return count;
+// };
 var removeElement = function(nums, val) {
-    let count = 0;
+    let counter = 0;
     for(let i = 0; i < nums.length; i++) {
-        if(nums[i] === null) {
-            return count;
-        }
-        if(nums[i] === val) {
-            nums.splice(i, 1);
-            i--;
-        } else {
-            count++;
+        if(nums[i] !== val) {
+            nums[counter] = nums[i];
+            counter++;
         }
     }
-    return count;
+    return counter;
 };
