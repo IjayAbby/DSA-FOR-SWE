2f1adb5aed1bdcc819284efc4764b231b0dd8a46
Ijay_dev <inyangasi4@gmail.com>
2023-05-20 00:45:40 +0300

    Time: 74 ms (19.59%), Space: 44.1 MB (40.22%) - LeetHub

--- 67-add-binary/67-add-binary.js
+++ 67-add-binary/67-add-binary.js
@@ -4,20 +4,20 @@
  * @return {string}
  */
 function addBinary(a, b) {
-    let result = [];
+    let result = "";
     let carry = 0;
     let i = a.length - 1;
     let j = b.length - 1;
     while(i >= 0 || j >= 0 || carry > 0) {
         let sum = carry;
         if(i >= 0) {
-            sum += +(a[i--]);
+            sum += parseInt(a[i--]);
         }
         if(j >= 0) {
-            sum += +(b[j--]);
+            sum += parseInt(b[j--]);
         }
-        result.push(sum % 2);
+        result = (sum % 2) + result;
         carry = Math.floor(sum / 2);
     }
-    return result.reverse().join('');
+    return result;
 };
