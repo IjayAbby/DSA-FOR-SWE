d6e77babf3d414afcd4c1a4f79c16dbf652a8878
Ijay_dev <inyangasi4@gmail.com>
2023-05-20 00:52:49 +0300

    Time: 55 ms (96.29%), Space: 45.5 MB (27.43%) - LeetHub

---
+++ 169-majority-element/169-majority-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function majorityElement(nums) {
+    let count = 0;
+    let candidate = null;
+    for(let num of nums) {
+        if(count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+    return candidate;
+};
