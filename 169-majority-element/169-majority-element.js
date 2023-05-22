342481a0b4c331a963c8d4a999db3d7ada78d726
Ijay_dev <inyangasi4@gmail.com>
2023-05-23 01:31:39 +0300

    Time: 57 ms (93.94%), Space: 44.1 MB (49.96%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -2,16 +2,29 @@
  * @param {number[]} nums
  * @return {number}
  */
-function majorityElement(nums) {
-    let count = 0;
-    let candidate = null;
-
-    for (let num of nums) {
-        if (count === 0) {
-            candidate = num;
+var majorityElement = function(nums) {
+    // Finding majority candidate
+    var count = 0;
+    candidate = -1;
+    for(var index = 0; index < nums.length; index++) {
+        if(count == 0) {
+            candidate = nums[index]
+            count = 1;
+        } else {
+            if(nums[index] == candidate)
+                count++;
+                else 
+                    count --;
         }
-        count += (num === candidate) ? 1 : -1;
     }
-
-    return candidate;
-}
+    // Checking if majority candidate occurs more than
+    // n/2 times
+        count = 0;
+        for(var index = 0; index < nums.length; index++) {
+            if(nums[index] == candidate)
+                count++;
+        }
+        if(count > (nums.length / 2))
+            return candidate;
+            return -1;
+};
