475034c7f50221b9f0085e2fcc26ed78d9664aa8
Ijay_dev <inyangasi4@gmail.com>
2023-08-17 23:46:55 +0300

    Time: 80 ms (68.23%), Space: 47.6 MB (92.77%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+function countBits(n) {
+    let result = Array(n + 1).fill(0);
+    let offset = 1;
+    for (let i = 1; i < n + 1; i++) {
+        if (offset * 2 === i) {
+            offset = i;
+        }
+        result[i] = 1 + result[i - offset];
+    }
+    return result;
+};
