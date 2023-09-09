1ebd6dccdf9efe4fe59d673139a7dedb5cf01f58
Ijay_dev <inyangasi4@gmail.com>
2023-09-09 17:49:00 +0300

    Time: 55 ms (85.39%), Space: 42.4 MB (58.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,16 +3,19 @@
  * @param {number} target
  * @return {number[]}
  nums = [20,10,2,15,11,7], target = 9
-               i   
-                  j 
- 
- 
+                       i  
+    object {key: value } 
+           d =  {20 :  0, 10: 1, 2: 2,15: 3, 11: 4} 
+            return [i, d[target-nums[i]]]
  */
 var twoSum = function(nums, target) {
+    let hashmap = {};
     for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++)
-            if (nums[i] + nums[j] === target) {
-                return [i,j]
-            }
+        let key = target - nums[i];
+        if(key in hashmap) {
+            return [i, hashmap[key]]
+        } else {
+            hashmap[nums[i]] = i;
+        }
     }
 };
