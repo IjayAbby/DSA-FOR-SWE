b3569494340f7c786c7ee76e803ee1a789b76dfc
Ijay_dev <inyangasi4@gmail.com>
2023-09-17 02:44:38 +0300

    Time: 4368 ms (5.43%), Space: 117.2 MB (5.43%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.js
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.js
@@ -2,26 +2,81 @@
  * @param {number[][]} heights
  * @return {number}
  */
-
-function minimumEffortPath(heights) {
-    const m = heights.length, n = heights[0].length;
-    const dir = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-    const dist = Array.from({length: m}, () => new Array(n).fill(Infinity));
-    dist[0][0] = 0;
-    const heap = [[0, 0, 0]]; // [distance, row, col]
+var minimumEffortPath = function(matrix) {
+    const rowC = matrix.length, colC = matrix[0].length
+    if (rowC === 1 && colC === 1)   return 0
     
-    while (heap.length) {
-        const [d, r, c] = heap.pop();
-        if (r === m - 1 && c === n - 1) return d;
-        for (let [dr, dc] of dir) {
-            const nr = r + dr, nc = c + dc;
-            if (nr < 0 || nc < 0 || nr === m || nc === n) continue;
-            const nd = Math.max(d, Math.abs(heights[nr][nc] - heights[r][c]));
-            if (nd < dist[nr][nc]) {
-                dist[nr][nc] = nd;
-                heap.push([nd, nr, nc]);
-                heap.sort((a, b) => b[0] - a[0]); // sort in descending order
+    
+    function Node(i, j) {
+        this.val = matrix[i][j]
+        this.effort = Infinity
+        
+        // neighNode => abs
+        this.neighs = new Map()
+    }
+    Node.table = new Array(rowC)
+    for (const i of Node.table.keys())
+        Node.table[i] = new Array(colC)
+        
+        
+    for (const [i, row] of matrix.entries()) {
+        for (const [j, val] of row.entries()) {
+            Node.table[i][j] = new Node(i, j)
+        }
+    }
+    
+    for (const [i, row] of matrix.entries()) {
+        for (const [j, val] of row.entries()) {
+            let node = Node.table[i][j], rightNode, downNode
+            
+            if (j + 1 < colC) {
+                rightNode = Node.table[i][j + 1]
+                const rightDiff = Math.abs(node.val - rightNode.val)
+                node.neighs.set(rightNode, rightDiff)
+                rightNode.neighs.set(node, rightDiff)
+            }
+            
+            if (i + 1 < rowC) {
+                downNode = Node.table[i + 1][j]
+                const downDiff = Math.abs(node.val - downNode.val)
+                node.neighs.set(downNode, downDiff)
+                downNode.neighs.set(node, downDiff)
             }
         }
     }
-}
+
+    
+    const startNode = Node.table[0][0], endNode = Node.table[-1 + rowC][-1 + colC]
+    startNode.effort = 0
+    const set = new Set([startNode])
+    
+    while (true) {
+        let bestEffort = Infinity, bestNeigh = null
+        
+        for (const node of set.values()) {
+            const nodeEffort = node.effort
+            for (const [neigh, diff] of node.neighs.entries()) {
+                const effort = Math.max(diff, nodeEffort)
+                if (effort < bestEffort) {
+                    bestEffort = effort
+                    bestNeigh = neigh
+                }
+            }
+        }
+        if (bestNeigh === endNode)  return bestEffort
+
+        for (const existing of bestNeigh.neighs.keys()) {
+            if (set.has(existing)) {
+                existing.neighs.delete(bestNeigh)
+                bestNeigh.neighs.delete(existing)
+                
+                if (!existing.neighs.size)
+                    set.delete(existing)
+            }
+        }
+        
+        bestNeigh.effort = bestEffort
+        if (bestNeigh.neighs.size)
+            set.add(bestNeigh)
+    }
+};
