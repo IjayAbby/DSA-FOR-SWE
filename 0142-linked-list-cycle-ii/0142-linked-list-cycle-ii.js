675a9859c0ade9a00beb368b1353a4be3126348a
Ijay_dev <inyangasi4@gmail.com>
2023-09-20 01:36:16 +0300

    Time: 71 ms (31.78%), Space: 45.3 MB (28.85%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// function ListNode(val) {
+//     this.val = val;
+//     this.next = null;
+// }
+
+function detectCycle(head) {
+    if (head === null || head.next === null) {
+        return null;
+    }
+
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            slow = head;
+            while (slow !== fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+
+    return null;
+}
