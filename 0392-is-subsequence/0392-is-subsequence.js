0ee660dd14cc680bf0f0eeb3b5412ef361f0c4a6
Ijay_dev <inyangasi4@gmail.com>
2023-09-23 02:11:05 +0300

    Time: 61 ms (14.83%), Space: 41.6 MB (86.70%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let i = 0;
+    let j = 0;
+    
+    while (i < s.length && j < t.length) {
+            if (s[i] === t[j]) {
+                i++;
+            } 
+            j++;
+    }
+    
+    if (i === s.length) {
+        return true;
+        } else {
+          return false;
+        }  
+};
