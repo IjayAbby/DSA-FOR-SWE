ad87bc12b2572baa9ff9801d033aca47fdee429f
Ijay_dev <inyangasi4@gmail.com>
2023-10-06 20:28:39 +0300

    Time: 57 ms (30.00%), Space: 42.3 MB (33.00%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function(n) {
+    if (n === 2) return 1;
+    if (n === 3) return 2;
+    
+    let product = 1;
+    while (n > 4) {
+        product *= 3;
+        n -= 3;
+    }
+    product *= n;
+    return product;
+};
