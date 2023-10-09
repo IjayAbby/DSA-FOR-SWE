15ec0fc3d2ba0934a6423eaf0b36e4356f42fa25
Ijay_dev <inyangasi4@gmail.com>
2023-10-09 12:24:28 +0300

    Time: 60 ms (14.62%), Space: 42.8 MB (24.93%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,40 +4,27 @@
  * @return {number[]}
  */
 function searchRange(nums, target) {
-    let result = [-1, -1];
-
-    // First binary search to find the first occurrence
-    let left = 0;
-    let right = nums.length - 1;
-
-    while (left <= right) {
-        let mid = Math.floor((right + left) / 2);
-        if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+    function findFirst(nums, target) {
+        let left = 0, right = nums.length - 1;
+        while (left < right) {
+            let mid = Math.floor(left + (right - left) / 2);
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid;
         }
+        return nums[left] === target ? left : -1;
     }
 
-    // If the target is not found, return [-1, -1]
-    if (left >= nums.length || nums[left] != target) {
-        return result;
-    }
-
-    result[0] = left;
-
-    // Second binary search to find the last occurrence
-    right = nums.length - 1;
-    while (left <= right) {
-        let mid = Math.floor((right + left) / 2);
-        if (nums[mid] <= target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+    function findLast(nums, target) {
+        let left = 0, right = nums.length - 1;
+        while (left < right) {
+            let mid = Math.floor(left + (right - left) / 2 + 1);
+            if (nums[mid] > target) right = mid - 1;
+            else left = mid;
         }
+        return nums[left] === target ? left : -1;
     }
 
-    result[1] = right;
-
-    return result;
+    if (!nums.length) return [-1, -1];
+    let first = findFirst(nums, target), last = findLast(nums, target);
+    return [first, last];
 }
