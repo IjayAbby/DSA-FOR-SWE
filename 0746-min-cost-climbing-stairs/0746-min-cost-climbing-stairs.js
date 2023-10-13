9917e84aea58241669df455719c682fc7ac7261c
Ijay_dev <inyangasi4@gmail.com>
2023-10-14 01:29:33 +0300

    Time: 62 ms (24.42%), Space: 42.6 MB (73.47%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+let minCostClimbingStairs = function(cost) {
+    for (let i = 2; i < cost.length; i++) {
+        cost[i] = cost[i] + Math.min(cost[i-2], cost[i-1]);
+    }
+    return Math.min(cost[cost.length - 2], cost[cost.length - 1]);
+};
