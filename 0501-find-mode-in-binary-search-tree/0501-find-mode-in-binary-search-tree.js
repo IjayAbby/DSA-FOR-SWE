63821a1736463e25d099b7eb664b76d4a964a94b
Ijay_dev <inyangasi4@gmail.com>
2023-11-02 02:21:07 +0300

    Time: 71 ms (61.34%), Space: 49.7 MB (55.04%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+function findMode(root) {
+    let max = 1;
+    let hash = {};
+    let result = [];
+    root && traverse(root);
+    for (let prop in hash) {
+        hash[prop] === max && result.push(prop);
+    }
+    return result;
+    
+    function traverse (node) {
+        if (hash[node.val]) {
+            hash[node.val] += 1;
+            max = Math.max(max, hash[node.val]);
+        } else {
+            hash[node.val] = 1;
+        };
+        node.left && traverse(node.left);
+        node.right && traverse(node.right);
+    };
+};
