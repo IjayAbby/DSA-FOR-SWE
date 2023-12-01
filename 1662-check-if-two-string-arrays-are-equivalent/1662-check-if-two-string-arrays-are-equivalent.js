2d10664d3044d165efa61bce777cb0247c34064b
Ijay_dev <inyangasi4@gmail.com>
2023-12-01 17:59:40 +0300

    Time: 47 ms (78.80%), Space: 42.2 MB (40.07%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -4,12 +4,5 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function(word1, word2) {
-    //let result = word1.concat(word2);
-    let firstArray = word1.join('')
-    let secondArray = word2.join('')
-    if (firstArray === secondArray) {
-        return true;
-    } else {
-        return false;
-    }
+    return word1.join('') === word2.join('');
 };
