4077cfe144a193ad1515eba4258de9abcd763419
Ijay_dev <inyangasi4@gmail.com>
2023-12-02 23:36:05 +0300

    Time: 71 ms (93.38%), Space: 49.8 MB (83.09%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.js
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.js
@@ -3,25 +3,26 @@
  * @param {string} chars
  * @return {number}
  */
-var countCharacters = function(words, chars) { 
-    let count = 0
-    const makeHash = (word) => {
-        let hash = {}
-        for (let i = 0; i < word.length; i++){
-            hash[word[i]] ? hash[word[i]]++ : hash[word[i]] = 1
-        }
-        return hash
-    }
-    let hash = makeHash(chars)    
-    for (let i = 0; i < words.length; i++){
-        let bool = true
-        let temp = makeHash(words[i])
-        for(let key in temp){
-            if (!hash[key] || hash[key] < temp[key]){
-                bool = false
+var countCharacters = function(words, chars) {
+    var res = 0;
+    words.forEach(word=>{
+        var isMatch = true;
+        var tempChars = chars;
+        for(let i = 0;i<word.length; i++) {
+            let l = word[i];
+            var ii = tempChars.indexOf(l);
+            if (ii>-1) {
+                // match 
+                tempChars = tempChars.substring(-1,ii) + tempChars.substring(ii+1,);
+            }else {
+                // not match
+                isMatch = false;
+                break;
             }
         }
-        bool && (count += words[i].length)
-    }
-    return count
+        if (isMatch) {
+            res+= word.length;
+        }
+    })
+    return res
 };
