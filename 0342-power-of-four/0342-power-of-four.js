cf00da09bdf8d86ab28b75e36ce41d4a0fc41336
Ijay_dev <inyangasi4@gmail.com>
2023-12-23 16:36:20 +0300

    Time: 67 ms (20.45%), Space: 43.8 MB (20.20%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.js
+++ 0342-power-of-four/0342-power-of-four.js
@@ -2,6 +2,24 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function(n) {
-   return n > 0 && (n & (n - 1)) === 0 && (n & 0xAAAAAAAA) === 0;
-};
+// var isPowerOfFour = function(n) {
+//    return n > 0 && (n & (n - 1)) === 0 && (n & 0xAAAAAAAA) === 0;
+// };
+
+// var reverseList = function(head, prev = null) {
+//     if(head === null) return prev;
+//     var next = head.next;
+//     head.next = prev;
+//     return reverseList(next, head);
+// }
+
+function isPowerOfFour(n) {
+    if (n <= 0) {
+        return false;
+    }
+    while (n % 4 === 0) {
+        n = n / 4;
+    }
+    return n === 1;
+}
+ 
